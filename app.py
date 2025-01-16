6e5fe3a92c696df48a0ba2727898002dcae77d35
Vimal Kumar .K.J <76762337+kickmyd2602@users.noreply.github.com>
2025-01-16 08:40:37 +0530

    Add logic to create tables in app.py

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
+from flask import Flask, request, jsonify 
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    salary = db.Column(db.Float, nullable=False)
+
+class Applicant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+    fname = db.Column(db.String(100), nullable=False)
+    lname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    resume = db.Column(db.Text, nullable=False)
+    
+@app.route('/jobs')
+def get_jobs():
+    jobs = Job.query.all()
+    data = [{"id":job.id, "title":job.title, "description":job.description, "location":location, "salary": salary} for job in jobs]
+    return jsonify(data)
+
+
 
